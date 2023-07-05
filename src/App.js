c0651682934c4c2597fbd21540dd9ad55a212bd2
Jawahar Balasubramani <jawaharbjkk@gmail.com>
2023-07-05 14:09:34 -0400

    chat-app

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Team-Chat-app.....Coming Soon....
-        </h1>
-      </header>
+      <head>
+        <title>Socket.IO chat</title>
+      </head>
+      <body>
+        <ul id="messages"></ul>
+        <form id="form" action="">
+          <input id="input" autocomplete="off" />
+          <button>Send</button>
+        </form>
+      </body>
     </div>
   );
 }
